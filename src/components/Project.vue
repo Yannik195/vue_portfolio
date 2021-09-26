979463489249f466c3da07cd852303c51f8499e5
Yannik195 <yannisim@gmail.com>
2021-09-26 22:47:09 +0200

    build

---
+++ src/components/Project.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div v-if="project.id % 2 == 1" class="wrapper-uneven">
+      <img
+        src="https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+      <div>
+        <h1>{{ project.name }}</h1>
+        <h2>{{ project.description }}</h2>
+        <Tag v-for="tag in project.tags" :key="tag" v-bind:tag="tag"></Tag>
+      </div>
+    </div>
+    <div v-else class="wrapper-even">
+      <div>
+        <h1>{{ project.name }}</h1>
+        <h2>{{ project.description }}</h2>
+        <Tag v-for="tag in project.tags" :key="tag" v-bind:tag="tag"></Tag>
+      </div>
+      <img
+        src="https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from "./Tag.vue";
+export default {
+  name: "Project",
+  props: ["project"],
+  components: {
+    Tag,
+  },
+};
+</script>
+
+<style scoped>
+.wrapper-uneven {
+  display: flex;
+  justify-content: center;
+}
+.wrapper-even {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  aspect-ratio: 16 / 9;
+  border-radius: 12px;
+  width: clamp(120px, 40vw, 800px);
+}
+
+@media only screen and (max-width: 900px) {
+  .wrapper-even {
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+  }
+  .wrapper-uneven {
+    flex-direction: column-reverse;
+    text-align: center;
+    align-items: center;
+  }
+
+  img {
+    width: 80%;
+  }
+}
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>#{{ tag }}</span>
+</template>
+
+<script>
+export default {
+  name: "Tag",
+  props: ["tag"],
+};
+</script>
+
+<style scoped>
+span {
+  color: #353a5a;
+  border: 2px #353a5a solid;
+  border-radius: 4px;
+  padding: 2px 4px;
+  margin: 0 4px 0 0;
+}
+</style>
